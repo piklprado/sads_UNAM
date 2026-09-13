8d731fbd677820e6694b03d714e2e6fe2a4416fc
piklprado <prado@ib.usp.br>
2026-09-12 23:27:30 -0300

    index.html quebrado

--- render_site.R
+++ render_site.R
@@ -3,4 +3,18 @@ library(xaringanExtra)
 library(distill)
 library(rmarkdown)
 
+rmarkdown::render(
+  input = "01_introduccion/slides.Rmd",
+  output_file = "slides.html",
+  output_dir = "01_introduccion",
+  encoding = "UTF-8"
+  )
+
+rmarkdown::render(
+  input = "02_ajustes/slides.Rmd",
+  output_file = "slides.html",
+  output_dir = "02_ajustes",
+  encoding = "UTF-8"
+)
+
 rmarkdown::render_site(encoding = 'UTF-8')
